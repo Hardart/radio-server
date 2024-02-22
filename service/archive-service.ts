c4959feea64f6f91079e0b748cc6dfba80869b59
Hardart <kaino@bk.ru>
2024-02-22 18:21:36 +0300

    change model

--- service/archive-service.ts
+++ service/archive-service.ts
@@ -3,7 +3,7 @@ import { ArchiveTrack } from '../models/ArchiveTrack'
 export class ArchiveTrackService {
   static async save(id: string) {
     const track = new ArchiveTrack({
-      date: new Date(),
+      createdAt: new Date(),
       trackId: id,
     })
     await track.save()
