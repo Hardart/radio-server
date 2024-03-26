60360f3838dfb4e9b9ae24236762ec9303a6b4e4
Hardart <kaino@bk.ru>
2024-03-26 22:18:38 +0300

    add models

---
+++ models/Program.ts
@@ -0,0 +1,26 @@
+import { Schema, model, InferSchemaType } from 'mongoose'
+
+const ProgramSchema = new Schema(
+  {
+    dayId: { type: Number, required: true },
+    title: { type: String, required: true },
+    description: String,
+    image: String,
+    hosts: [{ type: Schema.Types.ObjectId, ref: 'Host' }],
+    isReplay: { type: Boolean, default: false },
+    start: { type: String, required: true },
+    end: { type: String, required: true },
+  },
+  { timestamps: false, versionKey: false, toObject: { virtuals: true } }
+)
+
+ProgramSchema.set('toJSON', {
+  versionKey: false,
+  virtuals: true,
+  transform: function (_, ret) {
+    delete ret._id
+  },
+})
+
+export type Program = InferSchemaType<typeof ProgramSchema>
+export const Program = model('Program', ProgramSchema)
