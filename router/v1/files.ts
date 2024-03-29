ee68356a1a48577b0f9f83be55e34fe7dfb9fe79
Hardart <kaino@bk.ru>
2024-03-29 23:31:30 +0300

    refactoring

--- router/v1/files.ts
+++ router/v1/files.ts
@@ -5,11 +5,31 @@ import { resizeImage } from '../../middlewear/upload/image-resize-middlewear'
 import deleteAllImagesById from '../../middlewear/delete-all-middleware'
 import deleteOriginalImage from '../../middlewear/delete-original-middleware'
 import deleteFolder from '../../middlewear/delete-folder-middleware'
+import { asyncErrorHandler } from '../../handlers/error-handler'
 
 export default function fileRouter(router: Router) {
-  router.post('/image-avatar', imageUploadMiddlewear.single('avatar'), resizeImage, deleteOriginalImage, filesController.uploadTest)
-  router.post('/image-gallery', imageUploadMiddlewear.single('gallery'), resizeImage, deleteOriginalImage, filesController.uploadTest)
-  router.post('/image-news', imageUploadMiddlewear.single('news'), resizeImage, deleteOriginalImage, filesController.uploadTest)
-  router.post('/image-delete', deleteAllImagesById, filesController.deleteTest)
-  router.post('/folder-delete', deleteFolder, filesController.deleteTest)
+  router.post('/files', asyncErrorHandler(filesController.list))
+  router.post(
+    '/image-avatar',
+    imageUploadMiddlewear.single('avatar'),
+    resizeImage,
+    deleteOriginalImage,
+    asyncErrorHandler(filesController.upload)
+  )
+  router.post(
+    '/image-gallery',
+    imageUploadMiddlewear.single('gallery'),
+    resizeImage,
+    deleteOriginalImage,
+    asyncErrorHandler(filesController.upload)
+  )
+  router.post(
+    '/image-news',
+    imageUploadMiddlewear.single('news'),
+    resizeImage,
+    deleteOriginalImage,
+    asyncErrorHandler(filesController.upload)
+  )
+  router.post('/image-delete', deleteAllImagesById, asyncErrorHandler(filesController.delete))
+  router.post('/folder-delete', deleteFolder, asyncErrorHandler(filesController.delete))
 }
