74d421bb201f21e3029177ee5288616e26b06eb0
Hardart <kaino@bk.ru>
2024-06-06 00:34:23 +0300

    add base dir to images

--- service/file-service.ts
+++ service/file-service.ts
@@ -1,25 +1,28 @@
 import fs from 'fs'
 
 class FileService {
+  baseDir = process.env.NODE_EMV !== 'production' ? '/home' : '/Users/hardart/home'
   readImages(src: string) {
-    const baseSrc = src === '/images' ? `/Users/hardart/home${src}` : src
-    return foldersMap(baseSrc)
+    const baseSrc = !src.includes(this.baseDir) ? `${this.baseDir}${src}` : src
+    console.log(baseSrc)
+
+    return this.foldersMap(baseSrc)
+  }
+
+  private foldersMap(path: string): string[] {
+    const files = fs.readdirSync(path)
+    return files
+      .flatMap((f) => {
+        const fullPath = `${path}/${f}`
+        return fullPath.replace(this.baseDir, '')
+        return fullPath
+      })
+      .filter((path) => {
+        if (path.match(/\.(jpe?g|png|webp|avif)$/)) {
+          return path.includes('_preview') ? path : undefined
+        } else return !path.includes('.DS')
+      })
   }
 }
 
 export default new FileService()
-
-function foldersMap(path: string): string[] {
-  const files = fs.readdirSync(path)
-  return files
-    .flatMap((f) => {
-      const fullPath = `${path}/${f}`
-      // return fullPath.replace('/Users/hardart/home', '')
-      return fullPath
-    })
-    .filter((path) => {
-      if (path.match(/\.(jpe?g|png|webp|avif)$/)) {
-        return path.includes('_preview') ? path : undefined
-      } else return !path.includes('.DS')
-    })
-}
