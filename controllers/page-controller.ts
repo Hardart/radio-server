b57e784e8c25ce45edd080eaaa6e533783f0206b
Hardart <kaino@bk.ru>
2024-11-26 22:52:11 +0300

    feat: add new article-base route and newsData method to page controller

--- controllers/page-controller.ts
+++ controllers/page-controller.ts
@@ -1,6 +1,8 @@
 import type { Response, Request } from 'express'
 import BaseController from './base-controller'
 import contactService from '../service/contact-service'
+import tagService from '../service/tag-service'
+import categoryService from '../service/category-service'
 
 class PageController extends BaseController {
   async baseData(req: Request, res: Response) {
@@ -8,6 +10,12 @@ class PageController extends BaseController {
     const emails = await contactService.mailsList()
     const addresses = await contactService.addressesList()
     res.status(200).json(PageController.response({ phones, emails, addresses }))
+  }
+
+  async newsData(req: Request, res: Response) {
+    const tags = await tagService.list()
+    const categories = await categoryService.getAll()
+    res.status(200).json(PageController.response({ tags, categories }))
   }
 }
 
--- router/v1/articles.ts
+++ router/v1/articles.ts
@@ -3,8 +3,10 @@ import articleController from '../../controllers/article-controller'
 import authMiddleware from '../../middlewear/auth-middleware'
 import { asyncErrorHandler } from '../../handlers/error-handler'
 import { checkId } from '../../middlewear/mongoose-middleware'
+import pageController from '../../controllers/page-controller'
 
 function articlesRouter(router: Router) {
+  router.post('/article-base', asyncErrorHandler(pageController.newsData))
   router.post('/article', checkId, asyncErrorHandler(articleController.oneById))
   router.post('/article-list', asyncErrorHandler(articleController.all))
   router.post('/article-add', authMiddleware, asyncErrorHandler(articleController.addOne))
