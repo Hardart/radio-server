eafb95eab596a3d9818b4393ce7891300babe876
Hardart <kaino@bk.ru>
2025-07-09 00:50:08 +0300

    refactor: change admin to superadmin in isAdminMiddlewear

--- middlewear/is-admin-middleware.ts
+++ middlewear/is-admin-middleware.ts
@@ -5,6 +5,6 @@ import AppError from '../handlers/error-handler'
 export default function (req: Request, res: Response, next: NextFunction) {
   const user = req.body.user
   if (!user) throw AppError.UnathorizedError()
-  if (!user.roles.includes('admin')) throw AppError.PermissonDenided()
+  if (!user.roles.includes('superadmin')) throw AppError.PermissonDenided()
   next()
 }
