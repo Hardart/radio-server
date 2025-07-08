b88b5715bec740f3047781185db1e61d366e9733
Hardart <kaino@bk.ru>
2025-07-09 00:49:06 +0300

    feat: add router version 2

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import routerV1 from './router/v1'
+import routerV2 from './router/v2'
 import { ErrorHandler } from './middlewear/error-middleware'
 import cors from 'cors'
 import cookerParser from 'cookie-parser'
@@ -12,6 +13,7 @@ app.use(express.static('assets'))
 app.use(cookerParser())
 app.use(cors({ credentials: true, origin: 'http://localhost:5173' }))
 app.use('/api/v1/dashboard', routerV1)
+app.use('/api/v2/dashboard', routerV2)
 
 app.use('*', otherRoutes)
 app.use(ErrorHandler)
